5298002ce3bf4fa623b71428c1735b11cfcc5dfa
Nutthapat Pongtanyavichai <contact@leomotors.me>
2025-04-17 00:37:56 +0700

    fix lint tsc

--- packages/db-chuni/drizzle.config.ts
+++ packages/db-chuni/drizzle.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "drizzle-kit";
 
-import { environment } from "./src/environment";
-
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/schema/index.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: environment.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
 });
