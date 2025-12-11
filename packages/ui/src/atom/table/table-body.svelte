5cbd2d88d55dd36a78621cf068d371cd8023ed37
Nutthapat Pongtanyavichai <contact@leomotors.me>
2025-12-11 16:28:46 +0700

    add ui components

---
+++ packages/ui/src/atom/table/table-body.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { cn, type WithElementRef } from "@repo/ui/utils";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLTableSectionElement>> = $props();
+</script>
+
+<tbody
+  bind:this={ref}
+  data-slot="table-body"
+  class={cn("[&_tr:last-child]:border-0", className)}
+  {...restProps}
+>
+  {@render children?.()}
+</tbody>
---
+++ packages/ui/src/atom/table/table-cell.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { HTMLTdAttributes } from "svelte/elements";
+
+  import { cn, type WithElementRef } from "@repo/ui/utils";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLTdAttributes> = $props();
+</script>
+
+<td
+  bind:this={ref}
+  data-slot="table-cell"
+  class={cn(
+    "whitespace-nowrap bg-clip-padding p-2 align-middle [&:has([role=checkbox])]:pe-0",
+    className,
+  )}
+  {...restProps}
+>
+  {@render children?.()}
+</td>
