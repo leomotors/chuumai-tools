bf6e5b8e977b9d5ee5df50723724336c0f2c4711
Nutthapat Pongtanyavichai <contact@leomotors.me>
2025-12-11 23:07:29 +0700

    chuni-music: separate functions

--- apps/chuni-music/src/index.ts
+++ apps/chuni-music/src/index.ts
@@ -12,11 +12,22 @@ if (command === "music") {
 
   const version = process.argv[3];
   await downloadMusicData(version);
+  console.log("\n✅ Download Music Data + Seed Level Completed\n");
+} else if (command === "constant") {
+  if (!process.argv[3]) {
+    console.log("Please provide a version");
+    process.exit(1);
+  }
+
+  const version = process.argv[3];
   await updateMusicConstant(version);
-  console.log("\n✅ All steps completed\n");
-  await db.$client.end();
-  console.log("✅ Database connection closed");
+  console.log(
+    "\n✅ Update Music Constant from arcade-songs.zetaraku.dev Completed\n",
+  );
 } else {
   console.log(`Unknown Command: ${command}`);
   process.exit(1);
 }
+
+await db.$client.end();
+console.log("✅ Database connection closed");
